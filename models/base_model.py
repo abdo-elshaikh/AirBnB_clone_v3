fad54f00f55678628110f729ae09c52bb7396cb6
abdo-elshaikh <abdo_mhmd1@hotmail.com>
2024-02-26 15:34:22 +0200

    update AirBnb

--- models/base_model.py
+++ models/base_model.py
@@ -35,7 +35,7 @@ class BaseModel:
         """Returns a string representation of the instance"""
         cls = (str(type(self)).split('.')[-1]).split('\'')[0]
         return '[{}] ({}) {}'.format(cls, self.id, self.__dict__)
-    
+
     def save(self):
         """Updates updated_at with current time when instance is changed"""
         from models import storage
